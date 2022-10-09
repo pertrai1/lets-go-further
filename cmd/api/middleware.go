2d4d8064309200edd33edb38c6cf96ae0b35806a
Rob Simpson <rsimpson2@me.com>
2022-10-09 18:43:24 -0400

    Refactor authentication

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -127,15 +127,23 @@ func (app *application) authenticate(next http.Handler) http.Handler {
 	})
 }
 
-func (app *application) requireActivatedUser(next http.HandlerFunc) http.HandlerFunc {
+func (app *application) requireAuthenticatedUser(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
 		user := app.contextGetUser(r)
 
 		if user.IsAnonymous() {
 			app.authenticationRequiredResponse(w, r)
 			return
 		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) requireActivatedUser(next http.HandlerFunc) http.HandlerFunc {
+	fn := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		user := app.contextGetUser(r)
 
 		if !user.Activated {
 			app.inactiveAccountResponse(w, r)
@@ -144,4 +152,6 @@ func (app *application) requireActivatedUser(next http.HandlerFunc) http.Handler
 
 		next.ServeHTTP(w, r)
 	})
+
+	return app.requireAuthenticatedUser(fn)
 }
